fc566743decb5e2f7be8ede1f01513cdb1840ccf
Chay Nabors <chaynabors@gmail.com>
2020-10-19 14:34:19 -0500

    cleanup and balancing

--- src/main.rs
+++ src/main.rs
@@ -52,25 +52,46 @@ fn get_action_index(combatant: &Combatant) -> usize {
     else { input }
 }
 
+fn process_damage(target: &mut Combatant, damage_type: DamageType, damage_value: u32) {
+    let defense_value = match damage_type {
+        DamageType::Fire => target.get_stat(Stat::FireDefense),
+        DamageType::Physical => target.get_stat(Stat::PhysicalDefense),
+    };
+
+    let absorbtion_value = match damage_type {
+        DamageType::Fire => target.get_stat(Stat::FireAbsorbtion),
+        DamageType::Physical => target.get_stat(Stat::PhysicalAbsorbtion),
+    };
+
+    if damage_value < defense_value { return; }
+    let damage_value = damage_value - defense_value;
+
+    if absorbtion_value > 100 { target.hp += std::cmp::min(target.hp_max - target.hp, damage_value * (absorbtion_value - 100) / 100); }
+    else { target.hp -= std::cmp::min(target.hp, damage_value * (100 - absorbtion_value) / 100); }
+}
+
 fn apply_damage(target: &mut Combatant, source: EffectSource, damage_type: DamageType, multiplier: u32, divisor: u32, turns_to_live: Option<u32>) {
-    let damage_value = damage_type.damage_from_source(target, source, multiplier, divisor);
-    let damage_reduction = damage_type.damage_reduction_from_target(target);
-    if damage_reduction > damage_value { return; }
+    let damage_value = match damage_type {
+        DamageType::Fire => match source {
+            EffectSource::None => 1,
+            EffectSource::Origin => target.get_stat(Stat::FireAttack),
+            EffectSource::Other(source) => source.get_stat(Stat::FireAttack),
+        },
+        DamageType::Physical => match source {
+            EffectSource::None => 1,
+            EffectSource::Origin => target.get_stat(Stat::PhysicalAttack),
+            EffectSource::Other(source) => source.get_stat(Stat::PhysicalAttack),
+        },
+    } * multiplier / divisor;
 
     match turns_to_live {
         Some(ttl) => target.damage_per_turn.push(DamagePerTurn { damage_type: damage_type, value: damage_value, turns_to_live: ttl }),
-        None => target.hp -= std::cmp::min(damage_value - damage_reduction, target.hp),
+        None => process_damage(target, damage_type, damage_value),
     };
 }
 
 fn apply_modifier(target: &mut Combatant, modifier: Modifier, stat: Stat) {
-    match stat {
-        Stat::Agility => target.agility_modifiers.push(modifier),
-        Stat::FireAttack => target.fire_attack_modifiers.push(modifier),
-        Stat::FireResistance => target.fire_resistance_modifiers.push(modifier),
-        Stat::PhysicalAttack => target.physical_attack_modifiers.push(modifier),
-        Stat::PhysicalResistance => target.physical_resistance_modifiers.push(modifier),
-    }
+    target.stat_modifiers[stat as usize].push(modifier);
 }
 
 fn simulate_combat(combatants: &mut [Combatant]) {
@@ -81,16 +102,25 @@ fn simulate_combat(combatants: &mut [Combatant]) {
     while living_count > 1 {
         let source_index = match turn_order.next() { Some(i) => *i, None => panic!() };
 
-        for damage_per_turn in &mut combatants[source_index].damage_per_turn { damage_per_turn.turns_to_live -= std::cmp::min(damage_per_turn.turns_to_live, 1) }
-        for modifier in &mut combatants[source_index].agility_modifiers { if let Some(ref mut turns_left) = modifier.turns_to_live { *turns_left -= std::cmp::min(*turns_left, 1) } }
-        for modifier in &mut combatants[source_index].fire_attack_modifiers { if let Some(ref mut turns_left) = modifier.turns_to_live { *turns_left -= std::cmp::min(*turns_left, 1) } }
-        for modifier in &mut combatants[source_index].fire_resistance_modifiers { if let Some(ref mut turns_left) = modifier.turns_to_live { *turns_left -= std::cmp::min(*turns_left, 1) } }
-        for modifier in &mut combatants[source_index].physical_attack_modifiers { if let Some(ref mut turns_left) = modifier.turns_to_live { *turns_left -= std::cmp::min(*turns_left, 1) } }
-        for modifier in &mut combatants[source_index].physical_resistance_modifiers { if let Some(ref mut turns_left) = modifier.turns_to_live { *turns_left -= std::cmp::min(*turns_left, 1) } }
+        // decrement the lifetime of DOTS
+        for damage_per_turn in &mut combatants[source_index].damage_per_turn {
+            damage_per_turn.turns_to_live -= std::cmp::min(damage_per_turn.turns_to_live, 1)
+        }
 
+        // decrement the lifetime of modifiers
+        for stat in 0..Stat::MaxValue as usize {
+            for modifier in &mut combatants[source_index].stat_modifiers[stat] {
+                if let Some(ref mut turns_left) = modifier.turns_to_live {
+                    *turns_left -= std::cmp::min(*turns_left, 1)
+                }
+            }
+        }
+
+        // get action
         let action_index = if source_index == 0 { get_action_index(&combatants[source_index]) } else { 0 };
         let action = combatants[source_index].actions[action_index];
 
+        // process action
         for sub_action in action.sub_actions {
             let target_index = (source_index + 1) % combatants.len();
 
@@ -114,21 +144,21 @@ fn simulate_combat(combatants: &mut [Combatant]) {
             }
         }
 
-        // Apply damage per turn post turn
+        // apply DOT
         for i in 0..combatants[source_index].damage_per_turn.len() {
             let damage_per_turn = combatants[source_index].damage_per_turn[i];
-            apply_damage(&mut combatants[source_index], EffectSource::None, damage_per_turn.damage_type, damage_per_turn.value, 1, None)
+            process_damage(&mut combatants[source_index], damage_per_turn.damage_type, damage_per_turn.value);
         }
 
-        // Remove modifiers and damage per turn if the turns_to_live count is 0
+        // remove dead DOTS
         combatants[source_index].damage_per_turn.retain(|damage_per_turn| damage_per_turn.turns_to_live > 0 );
-        combatants[source_index].agility_modifiers.retain(|modifier| match modifier.turns_to_live { Some(x) => x > 0, _ => true } );
-        combatants[source_index].fire_attack_modifiers.retain(|modifier| match modifier.turns_to_live { Some(x) => x > 0, _ => true } );
-        combatants[source_index].fire_resistance_modifiers.retain(|modifier| match modifier.turns_to_live { Some(x) => x > 0, _ => true } );
-        combatants[source_index].physical_attack_modifiers.retain(|modifier| match modifier.turns_to_live { Some(x) => x > 0, _ => true } );
-        combatants[source_index].physical_resistance_modifiers.retain(|modifier| match modifier.turns_to_live { Some(x) => x > 0, _ => true } );
 
-        // Calculate living to determine winner
+        // remove dead modifiers
+        for stat in 0..Stat::MaxValue as usize {
+            combatants[source_index].stat_modifiers[stat].retain(|modifier| match modifier.turns_to_live { Some(x) => x > 0, _ => true } );
+        }
+
+        // determine winner or continue
         living_count = 0;
         combatants.iter().for_each(|combatant| if combatant.alive() { living_count += 1; });
     }
@@ -153,40 +183,9 @@ fn main() -> std::io::Result<()> {
         sub_actions: &[
             SubAction {
                 effects: &[
-                    Effect::Damage(DamageType::Fire, 5, 1, Some(3)),
+                    Effect::Damage(DamageType::Fire, 2, 3, Some(3)),
                 ],
                 target_flags: &[&[TargetFlag::Any]],
-                target_count: 1,
-            },
-        ],
-    };
-    
-    let beat_female = Action {
-        display_name: "Beat female",
-        sub_actions: &[
-            SubAction {
-                effects: &[
-                    Effect::Damage(DamageType::Physical, 2, 1, None),
-                ],
-                target_flags: &[
-                    &[TargetFlag::Gender(Gender::Female)]
-                ],
-                target_count: 1,
-            },
-            SubAction {
-                effects: &[
-                    Effect::Modifier(
-                        Modifier {
-                            modifier_type: ModifierType::Subtract, 
-                            value: 5,
-                            turns_to_live: Some(1),
-                        },
-                        Stat::PhysicalAttack,
-                    ),
-                ],
-                target_flags: &[
-                    &[TargetFlag::Origin]
-                ],
                 target_count: 1,
             },
         ],
@@ -219,23 +218,14 @@ fn main() -> std::io::Result<()> {
     let brayden = Combatant {
         name: "Brayden",
         gender: Gender::Male,
-        actions: &[&attack, &beat_female, &skip],
+        actions: &[&attack, &skip],
 
         hp: 70,
         hp_max: 70,
         damage_per_turn: vec![],
 
-        agility: 12,
-        fire_attack: 0,
-        fire_resistance: 0,
-        physical_attack: 26,
-        physical_resistance: 9,
-
-        agility_modifiers: vec![],
-        fire_attack_modifiers: vec![],
-        fire_resistance_modifiers: vec![],
-        physical_attack_modifiers: vec![],
-        physical_resistance_modifiers: vec![],
+        stats: [12, 11, 0, 2, 67, 17, 7],
+        stat_modifiers: [vec![], vec![], vec![], vec![], vec![], vec![], vec![]],
     };
 
     let chay = Combatant {
@@ -247,17 +237,8 @@ fn main() -> std::io::Result<()> {
         hp_max: 46,
         damage_per_turn: vec![],
 
-        agility: 26,
-        fire_attack: 2,
-        fire_resistance: 1000,
-        physical_attack: 17,
-        physical_resistance: 12,
-
-        agility_modifiers: vec![],
-        fire_attack_modifiers: vec![],
-        fire_resistance_modifiers: vec![],
-        physical_attack_modifiers: vec![],
-        physical_resistance_modifiers: vec![],
+        stats: [26, 126, 16, 0, 8, 6, 3],
+        stat_modifiers: [vec![], vec![], vec![], vec![], vec![], vec![], vec![]],
     };
 
     let combatants = &mut vec![brayden, chay];
